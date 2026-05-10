805309ef6ba3e43e9fcbeed25392abddc342a66e
D-practice <phanminhduy1532006@gmail.com>
2026-05-10 21:07:39 +0700

    Implement inference and system evaluation pipelines with integrated visualization tools

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from preprocess.general_preprocess import run_preprocessing_pipeline
 from segment.general_segmentation import run_segmentation_pipeline
 from datalayer.build_dataset import run_dataset_pipeline
 from model.train import run_training_pipeline
+from model.inference import run_inference_demo
+from model.evaluate import evaluate_system
 
 def main():
     parser = argparse.ArgumentParser(description="Captcha processing pipeline")
     parser.add_argument(
         "--stage", type=str, required=True,
-        choices=["0", "1", "2", "3", "4", "5"],
+        choices=["0", "1", "2", "3", "4", "5", "6", "7"],
         help="Choose a stage to run"
     )
 
@@ -33,6 +35,12 @@ def main():
         # 5. Train model
         run_training_pipeline()
         
+        # 6. Inference Demo
+        run_inference_demo()
+        
+        # 7. Evaluation
+        evaluate_system()
+        
     elif args.stage == "1":
         # Change depends on dataset
         process_1k_pbm_dataset()
@@ -44,6 +52,10 @@ def main():
         run_dataset_pipeline()
     elif args.stage == "5":
         run_training_pipeline()
+    elif args.stage == "6":
+        run_inference_demo()
+    elif args.stage == "7":
+        evaluate_system()
 
 
 if __name__ == "__main__":
